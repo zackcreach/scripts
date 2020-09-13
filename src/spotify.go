a1a09a7fcf344d523079221d4706e5352837b25d
Zack Creach <zackcreach@gmail.com>
2020-09-13 19:47:49 -0400

    Spotify scripts updated.

---
+++ src/spotify.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+  "io"
+  "io/ioutil"
+  "log"
+	"flag"
+	"os"
+  "strings"
+  "regexp"
+  "encoding/json"
+)
+
+var SPOTIFY_TOKEN_FILENAME string = generateFilepath("spotify_access_token.txt")
+const HOST string = "https://api.spotify.com/v1/me/player"
+const VALUE_DEFAULT int = 5
+
+var action string
+var option string
+var value int
+
+type RefreshTokenResponse struct {
+  AccessToken string `json:"access_token"`
+  TokenType string `json:"token_type"`
+  ExpiresIn int `json:"expires_in"`
+  Scope string `json:"scope"`
+}
+
+type UnauthorizedResponse struct {
+   Status int `json:"status"`
+}
+
+type CurrentlyPlayingResponse struct {
+  IsPlaying bool `json:"is_playing"`
+}
+
+type DevicesResponse struct {
+  Name bool `json:"name"`
+  IsPlaying bool `json:"name.is_playing"`
+}
+
+func init() {
+	flag.StringVar(&action, "action", "", "Action flag such as 'play'")
+	flag.StringVar(&option, "option", "", "Option flag to specify the device, for example")
+	flag.IntVar(&value, "value", 5, "Value flag corresponding to the option (e.g. 10)")
+	flag.Parse()
+}
+
+func generateFilepath(filename string) string {
+  cwd, err := os.Getwd();
+
+  if err != nil {
+    log.Print("Error: cannot get working directory")
+  }
+
+  return cwd + "/" + filename
+}
+
+func request(method string, path string, options io.Reader, headers map[string]string) []byte {
+  client := &http.Client{}
+
+  var endpoint string
+
+  found, _ := regexp.MatchString("^/", path)
+
+  if found {
+    endpoint = HOST + path
+  } else {
+    endpoint = path
+  }
+
+	req, _ := http.NewRequest(method, endpoint, options)
+
+  // Add passed in headers to request, otherwise assume auth header is needed
+  if headers != nil {
+    for key, value := range headers {
+      req.Header.Add(key, value)
+    }
+  } else {
+    tokenInFile, err := ioutil.ReadFile(SPOTIFY_TOKEN_FILENAME)
+    if err != nil {
+      log.Print("Error: Failed to read token from file: ", err)
+      return nil
+    }
+    
+    var accessToken string = strings.ReplaceAll(string(tokenInFile), "\n", "")
+    req.Header.Add("Authorization", "Bearer " + accessToken)
+  }
+
+  res, err := client.Do(req)
+
+  // Handle general request errors
+  if err != nil {
+    log.Print("Error: Failed to request url: ", endpoint, err)
+    return nil
+  }
+
+  // Handle 401 error (token refresh needed)
+  if res.StatusCode == 401 {
+    updateToken(method, path, options, headers)
+    return nil
+  }
+
+  bytes, err := ioutil.ReadAll(res.Body)
+
+  // Handle bytes (body) read errors
+  if err != nil {
+    log.Print("Error: Failed to read bytes: ", err)
+  }
+
+  defer res.Body.Close()
+  return bytes
+}
+
+func updateToken(method string, path string, options io.Reader, headers map[string]string) {
+  refreshEndpoint := "https://accounts.spotify.com/api/token"
+  
+  values := url.Values{}
+  values.Add("grant_type", "refresh_token")
+  values.Add("client_id", os.Getenv("SPOTIFY_CLIENT_ID"))
+  values.Add("client_secret", os.Getenv("SPOTIFY_CLIENT_SECRET"))
+  values.Add("refresh_token", os.Getenv("SPOTIFY_TOKEN_REFRESH"))
+
+  refreshData := strings.NewReader(values.Encode())
+
+  refreshHeaders := map[string]string{
+    "content-type": "application/x-www-form-urlencoded",
+  }
+
+  response := request("POST", refreshEndpoint, refreshData, refreshHeaders)
+
+  var tokenData RefreshTokenResponse
+  if err := json.Unmarshal(response, &tokenData); err != nil {
+    log.Print("Error: Failed to unmarshall response: ", err)
+  }
+
+  newToken := tokenData.AccessToken
+  fmt.Println("Token refreshed: ", newToken)
+
+  if err := ioutil.WriteFile(SPOTIFY_TOKEN_FILENAME, []byte(newToken), 0644); err != nil {
+    log.Print("Error: Failed to write file: ", err)
+  }
+
+  request(method, path, options, headers)
+}
+
+func main() {
+	switch action {
+    case "devices":
+      response := request("GET", "/devices", nil, nil)
+        fmt.Println(string(response))
+    case "playing":
+      fmt.Println("Playing")
+    case "previous":
+      fmt.Println("Previous")
+    case "next":
+      fmt.Println("Next")
+    case "play":
+      currentlyPlayingResponse := request("GET", "/currently-playing", nil, nil)
+
+      var currentlyPlaying CurrentlyPlayingResponse
+      if err := json.Unmarshal(currentlyPlayingResponse, &currentlyPlaying); err != nil {
+        log.Print("Error: Failed to unmarshal response: ", err)
+      }
+
+      if (currentlyPlaying.IsPlaying == true) {
+        request("PUT", "/pause", nil, nil)
+      } else {
+        request("PUT", "/play", nil, nil)
+      }
+    case "transfer":
+      fmt.Println("Transfer")
+    case "volume":
+      fmt.Println("Volume")
+    case "reinstall":
+      fmt.Println("Reinstall")
+    default:
+      fmt.Println("Default")
+	}
+}
---
+++ src/spotify.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+# NOTE: requires json library (install using 'npm i -g json')
+
+ARG1=$1
+ARG2=$2
+ARG3=$3
+
+SPOTIFY_TOKEN_FILENAME='spotify_access_token.txt'
+SPOTIFY_TOKEN_LOCAL=$(cat $SPOTIFY_TOKEN_FILENAME)
+VOLUME_VALUE_DEFAULT=5
+
+request() {
+  local method=$1
+  local path=$2
+  local options=$3
+  local host='https://api.spotify.com/v1/me/player'
+
+  local response=$(curl --request $method "$host$path" \
+    --header "Authorization: Bearer $SPOTIFY_TOKEN_LOCAL" \
+    $options
+  )
+  local error=$(echo $response | json -q error)
+
+  if [[ $error ]]; then
+    local status=$(echo $error | json -q status)
+
+    # Handle 401 expired error
+    if [[ $status == 401 ]]; then
+      SPOTIFY_TOKEN_LOCAL=$(update_token)
+      echo $(curl --request $method "$host$path" \
+        --header "Authorization: Bearer $SPOTIFY_TOKEN_LOCAL" \
+        $options
+      )
+    fi
+  else
+    echo $response
+  fi
+}
+
+update_token() {
+  # Request new token based on refresh_token and save response
+  local response=$(curl --request POST \
+    --url https://accounts.spotify.com/api/token \
+    --data grant_type=refresh_token \
+    --data client_id=$SPOTIFY_CLIENT_ID \
+    --data client_secret=$SPOTIFY_CLIENT_SECRET \
+    --data refresh_token=$SPOTIFY_TOKEN_REFRESH \
+    --header 'content-type: application/x-www-form-urlencoded')
+
+  # Extract access_token value from response JSON
+  local new_token=$(echo $response | json -q access_token)
+
+  # Write new token to .zshrc for next script execution
+  # perl -pi -e "s/(?<=SPOTIFY_TOKEN_ACTIVE=)(.+)?/\"$new_token\"/" $HOME/.zshrc
+  $new_token > $SPOTIFY_TOKEN_FILENAME
+
+  # Return new token
+  echo $new_token
+}
+
+case $ARG1 in
+  'devices')
+    devices=$(request 'GET' '/devices' | json -q devices)
+if [[ $ARG2 ]]; then
+      echo $devices | json -q -c "this.name === '$ARG2'"
+    else
+      echo $devices | json -q
+    fi
+    ;;
+
+  'playing')
+    response=$(request 'GET' '/currently-playing')
+    echo $response | json -q 
+    ;;
+
+  'previous')
+    response=$(request 'POST' '/previous')
+    ;;
+
+  'next')
+    response=$(request 'POST' '/next')
+    ;;
+
+  'play')
+    is_playing=$(request 'GET' '/currently-playing' | json -q is_playing)
+    if [[ $is_playing == true ]]; then
+      response=$(request 'PUT' '/pause')
+    elif [[ $is_playing == false ]]; then
+      response=$(request 'PUT' '/play')
+    fi
+    ;;
+
+  'transfer')
+    response=$(request 'GET' '/devices')
+    devices=$(echo $response | json -q devices)
+
+    if [[ $ARG2 ]]; then
+      device_id=$(echo $devices | json -q -c "this.name === \"$ARG2\"" | json -q [0]id)
+    else
+      device_id=$(echo $devices | json -q [0]id)
+    fi
+
+    request 'PUT' '' "--header Content-Type:application/json \
+      --header Accept:application/json \
+      --data {\"device_ids\":[\"$device_id\"]}"
+    ;;
+
+  'volume')
+    # Set direction to modify volume
+    if [[ $ARG2 == 'up' || $ARG2 == 'down' || $ARG2 == 'mute' ]]; then
+      direction=$ARG2
+    else
+      echo 'Error: Specify either \'up\' or \'down\' as second argument
+    fi
+
+    # Set custom value to increment/decrement, defaulting to 5
+    if [[ $ARG3 =~ [0-9]+ ]]; then
+      value=$ARG3
+    else
+      value=$VOLUME_VALUE_DEFAULT
+    fi
+
+    # Get devices list and then filter by active devices to find volume percent
+    devices=$(request 'GET' '/devices' | json -q devices)
+    volume_percent=$(echo $devices | json -q -c 'this.is_active' | json -q [0]volume_percent)
+
+    if [[ $direction == 'up' ]]; then
+      volume_percent_modified=$(($volume_percent + $value))
+    elif [[ $direction == 'down' ]]; then
+      volume_percent_modified=$(($volume_percent - $value))
+    elif [[ $direction == 'mute' ]]; then
+      volume_percent_modified=0
+    fi
+
+    volume_percent_query="?volume_percent=$volume_percent_modified"
+    response=$(request 'PUT' "/volume$volume_percent_query")
+    ;;
+  
+  'reinstall')
+    src_path="$HOME/scripts/src/spotify.sh"
+    bin_path="$HOME/scripts/bin/spotify"
+
+    sudo cp $src_path $bin_path
+    if [[ $? == 0 ]]; then
+      echo "Successfully reinstalled at $bin_path"
+    fi
+    ;;
+esac
